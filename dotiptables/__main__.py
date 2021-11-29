40391f9171e5fd342f5667ef1b2e0c9941861057
slavniyteo <slavniyteo@gmail.com>
2021-11-29 10:46:09 +0300

    args: set default output dir to ./out & create if absent
    Signed-off-by: slavniyteo <slavniyteo@gmail.com>

--- dotiptables/__main__.py
+++ dotiptables/__main__.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
 
-import os
+import argparse
 import errno
-import sys
+import os
 import re
 import subprocess
-import argparse
-import pprint
+import sys
 
 import jinja2
-from jinja2 import Template
 from jinja2.loaders import PackageLoader
 
 BUILTIN_CHAINS = [
@@ -64,41 +62,43 @@ BUILTIN_TARGETS = [
     'ULOG',
 ]
 
-
 env = jinja2.Environment(
-        loader=PackageLoader('dotiptables', 'templates'))
+    loader=PackageLoader('dotiptables', 'templates'))
 
-re_table='''^\*(?P<table>\S+)'''
+re_table = '''^\*(?P<table>\S+)'''
 re_table = re.compile(re_table)
 
-re_chain='''^:(?P<chain>\S+) (?P<policy>\S+) (?P<counters>\S+)'''
+re_chain = '''^:(?P<chain>\S+) (?P<policy>\S+) (?P<counters>\S+)'''
 re_chain = re.compile(re_chain)
 
-re_rule='''^(\[(?P<packets>\d+):(?P<bytes>\d+)\] )?-A (?P<chain>\S+)( (?P<conditions>.*))?( -(?:j|g) (?P<target>\S*))( (?P<extra>.*))?'''
+re_rule = '''^(\[(?P<packets>\d+):(?P<bytes>\d+)\] )?-A (?P<chain>\S+)( (?P<conditions>.*))?( -(?:j|g) (?P<target>\S*))( (?P<extra>.*))?'''
 re_rule = re.compile(re_rule)
 
-re_commit='''^COMMIT'''
-re_commit=re.compile(re_commit)
+re_commit = '''^COMMIT'''
+re_commit = re.compile(re_commit)
 
-re_comment='''^#(?P<comment>.*)'''
-re_comment=re.compile(re_comment)
+re_comment = '''^#(?P<comment>.*)'''
+re_comment = re.compile(re_comment)
 
 
 def parse_args():
     p = argparse.ArgumentParser()
-    p.add_argument('--outputdir', '-d', default='.')
+    p.add_argument('--outputdir', '-d', default='./out')
     p.add_argument('--render', action='store_true')
     p.add_argument('input', nargs='?')
 
     return p.parse_args()
 
+
 def stripped(fd):
     for line in fd:
         yield line.strip()
 
+
 def handle_table(iptables, mo, line):
     iptables[mo.group('table')] = {}
     iptables['_table'] = iptables[mo.group('table')]
+
 
 def handle_chain(iptables, mo, line):
     policy = mo.group('policy')
@@ -106,33 +106,36 @@ def handle_chain(iptables, mo, line):
         policy = None
 
     iptables['_table'][mo.group('chain')] = {
-            'policy': policy,
-            'rules': [],
-            'targets': set(),
-            }
+        'policy': policy,
+        'rules': [],
+        'targets': set(),
+    }
+
 
 def handle_rule(iptables, mo, line):
-    fields = dict( (k, v if v else '') for k,v in mo.groupdict().items())
+    fields = dict((k, v if v else '') for k, v in mo.groupdict().items())
     iptables['_table'][fields['chain']]['rules'].append(fields)
 
     if mo.group('target') and mo.group('target') in iptables['_table']:
         iptables['_table'][fields['chain']]['targets'].add(mo.group('target'))
 
+
 def handle_commit(iptables, mo, line):
     iptables['_table'] = None
 
+
 def read_chains(input):
     iptables = {
-            '_table': None,
-            }
+        '_table': None,
+    }
 
     actions = (
-            (re_table,   handle_table),
-            (re_chain,   handle_chain),
-            (re_rule,    handle_rule),
-            (re_commit,  handle_commit),
-            (re_comment, None),
-            )
+        (re_table, handle_table),
+        (re_chain, handle_chain),
+        (re_rule, handle_rule),
+        (re_commit, handle_commit),
+        (re_comment, None),
+    )
 
     for line in stripped(input):
         try:
@@ -150,6 +153,7 @@ def read_chains(input):
 
     del iptables['_table']
     return iptables
+
 
 def output_rules(iptables, opts):
     tmpl = env.get_template('rules.html')
@@ -175,6 +179,7 @@ def output_rules(iptables, opts):
                     rules=data['rules'],
                     policy=data['policy']))
 
+
 def output_dot_table(iptables, opts, table):
     tmpl = env.get_template('table.dot')
 
@@ -182,8 +187,9 @@ def output_dot_table(iptables, opts, table):
         fd.write(tmpl.render(
             table=table,
             chains=iptables[table],
-            ))
+        ))
         fd.write('\n')
+
 
 def output_dot(iptables, opts):
     tmpl = env.get_template('index.html')
@@ -194,22 +200,27 @@ def output_dot(iptables, opts):
         output_dot_table(iptables, opts, table)
         continue
 
+
 def render_svg(iptables, opts):
     for table in iptables:
         p = subprocess.Popen(['dot', '-T', 'svg', '-o',
-                os.path.join(opts.outputdir, '%s.svg' % table),
-                os.path.join(opts.outputdir, '%s.dot' % table)])
+                              os.path.join(opts.outputdir, '%s.svg' % table),
+                              os.path.join(opts.outputdir, '%s.dot' % table)])
         p.communicate()
+
 
 def main():
     opts = parse_args()
 
     if not os.path.isdir(opts.outputdir):
-        print((
-                'ERROR: output directory %s does not exist.' %
-                (opts.outputdir)
-                ), file=sys.stderr)
-        sys.exit(1)
+        try:
+            os.mkdir(opts.outputdir, 0o755)
+            print('Output directory created: %s' % opts.outputdir)
+        except:
+            print((
+                'ERROR: can''t created output directory: %s' % opts.outputdir
+            ), file=sys.stderr)
+            sys.exit(1)
 
     print('Reading iptables data.')
     with (open(opts.input, 'r') if opts.input else sys.stdin) as fd:
@@ -223,6 +234,6 @@ def main():
         print('Generating SVG output.')
         render_svg(iptables, opts)
 
-if __name__ == '__main__':
-        main()
 
+if __name__ == '__main__':
+    main()
